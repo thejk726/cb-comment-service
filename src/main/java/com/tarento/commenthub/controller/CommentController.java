68879dacd5c186fd0ee43fdf158073ef180eda94
ruksanasemeir <ruksana.semeir@tarento.com>
2024-10-21 18:33:09 +0530

    Changed the response status of search API incase of commentTree Not found

--- src/main/java/com/tarento/commenthub/controller/CommentController.java
+++ src/main/java/com/tarento/commenthub/controller/CommentController.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -117,6 +118,9 @@ public class CommentController {
   @PostMapping("/search")
   public ResponseEntity<?> search(@RequestBody SearchCriteria searchCriteria) {
     ApiResponse response = commentService.paginatedComment(searchCriteria);
+    if (response.getResponseCode().equals(HttpStatus.NOT_FOUND) && response.getResult().isEmpty()) {
+      return new ResponseEntity<>(response, HttpStatus.OK);
+    }
     return new ResponseEntity<>(response, response.getResponseCode());
   }
   @PostMapping("/list")
